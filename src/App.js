dad93fbfeca78d9f979cdad16d53df965076ea3c
juan silva <juan_silva_o@hotmail.com>
2022-10-07 16:01:52 -0300

    react-bootstrap bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
+import NotFound from './views/NotFound';
+
 
 function App() {
   return (
-    <div >
-      Hola
+      <div >
+          <Routes>
+            <Route path='/' element={<Home /> } />
+            <Route path='*' element={<NotFound />}/>
+            </Routes>
     </div>
   );
 }
