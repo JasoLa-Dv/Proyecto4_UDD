2c20a9fbaa48820bcbbe14bc42838b22b76f6fa2
juan silva <juan_silva_o@hotmail.com>
2022-10-07 17:39:40 -0300

    react-loading-skeleton

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -1,6 +1,6 @@
 import Footer  from '../components/Footer'
-
-
+import './style.css' ;
+import Meal from '../components/Meal';
 
 
 
@@ -8,7 +8,7 @@ const Menu = () => {
     return(
         <div>
         <h1 className='text-center mt-3'>Menu</h1>
-      
+       <Meal />
         <Footer />
     </div>
 )
